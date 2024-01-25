ceb623783c0a9ab4d9fa13f40930035151b1b779
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-25 12:02:42 -0800

    add get cities test

---
+++ test/CdekSample.Test/Token.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample.Test;
+
+public sealed record class Token
+(
+  [property: JsonPropertyName("access_token")] string AccessToken,
+  [property: JsonPropertyName("token_type")]   string TokenType,
+  [property: JsonPropertyName("expires_in")]   int    ExpiresIn,
+  [property: JsonPropertyName("scope")]        string Scope,
+  [property: JsonPropertyName("jti")]          string Jti
+);
