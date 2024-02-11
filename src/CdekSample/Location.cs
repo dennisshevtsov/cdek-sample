9d4b7dbaad8cbffae3dabc4f4cdfce9dfa887e74
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-11 03:59:38 -0800

    add location

---
+++ src/CdekSample/Location.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class Location
+(
+  [property: JsonPropertyName("country_code")] string  CountryCode,
+  [property: JsonPropertyName("region_code")]  int     RegionCode,
+  [property: JsonPropertyName("region")]       string  Region,
+  [property: JsonPropertyName("city_code")]    int     CityCode,
+  [property: JsonPropertyName("city")]         string  City,
+  [property: JsonPropertyName("fias_guid")]    Guid?   FiasGuid,
+  [property: JsonPropertyName("postal_сode")]  string? PostalCode,
+  [property: JsonPropertyName("longitude")]    float   Longitude,
+  [property: JsonPropertyName("latitude")]     float   Latitude,
+  [property: JsonPropertyName("address")]      string  Address,
+  [property: JsonPropertyName("address_full")] string  AddressFull
+);
