b640cb7d81480bf0d0c6acec22b00622e45ef4d3
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-11 04:23:08 -0800

    add dimansion

---
+++ src/CdekSample/Dimension.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class Dimension
+(
+  [property: JsonPropertyName("width")]  float Width,
+  [property: JsonPropertyName("height")] float Height,
+  [property: JsonPropertyName("depth")]  float Depth
+);
