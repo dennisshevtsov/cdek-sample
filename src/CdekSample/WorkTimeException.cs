2edd3b35fdc9c3f7c47c540f81d8084ffd2f5188
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-11 04:23:39 -0800

    add work time exception

---
+++ src/CdekSample/WorkTimeException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class WorkTimeException
+(
+  [property: JsonPropertyName("date")]       DateTimeOffset Date,
+  [property: JsonPropertyName("time")]       string?        Time,
+  [property: JsonPropertyName("is_working")] bool           IsWorking
+);
