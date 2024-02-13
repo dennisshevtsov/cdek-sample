6a338efe3a88eaceaa8fba5c4ba273420ff8e6c6
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-13 11:40:58 -0800

    add package

---
+++ src/CdekSample/Package.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class Package
+(
+  [property: JsonPropertyName("weight")] int  Weight,
+  [property: JsonPropertyName("length")] int? Length,
+  [property: JsonPropertyName("width")]  int? Width,
+  [property: JsonPropertyName("height")] int? Height
+);
