d869c1fcfb1a55943c6d7e949f964045a8efac37
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-13 11:48:29 -0800

    add tariff code

---
+++ src/CdekSample/TariffCode.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class TariffCode
+(
+  [property: JsonPropertyName("tariff_code")]        int    Code,
+  [property: JsonPropertyName("tariff_name")]        string Name,
+  [property: JsonPropertyName("tariff_description")] string Description,
+  [property: JsonPropertyName("delivery_mode")]      int    DeliveryMode,
+  [property: JsonPropertyName("delivery_sum")]       float  DeliverySum,
+  [property: JsonPropertyName("period_min")]         int    PeriodMin,
+  [property: JsonPropertyName("period_max")]         int    PeriodMax,
+  [property: JsonPropertyName("calendar_min")]       int?   CalendarMin,
+  [property: JsonPropertyName("calendar_max")]       int?   CalendarMax
+);
