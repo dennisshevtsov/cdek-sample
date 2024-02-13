e060b223fea021a4491946f617deb788ded89e70
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-13 11:48:45 -0800

    add calculator by tariff list response

---
+++ src/CdekSample/CalculatorByTariffListResponse.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class CalculatorByTariffListResponse
+(
+  [property: JsonPropertyName("tariff_codes")] TariffCode[]? TariffCodes
+);
