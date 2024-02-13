b8594429157743761a4d30c03b3a9badb3ed7293
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-13 11:41:18 -0800

    add caculator location

---
+++ src/CdekSample/CalculatorLocation.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class CalculatorLocation
+(
+  [property: JsonPropertyName("code")]         int     Code,
+  [property: JsonPropertyName("postal_code")]  string  PostalCode,
+  [property: JsonPropertyName("country_code")] string? CountryCode,
+  [property: JsonPropertyName("city")]         string? City,
+  [property: JsonPropertyName("address")]      string  Address
+);
