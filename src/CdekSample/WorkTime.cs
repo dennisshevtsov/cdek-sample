3b9ba87f15d13d79d3cc26a4f0a231deb622e727
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-17 12:18:51 -0800

    add comments to work time

--- src/CdekSample/WorkTime.cs
+++ src/CdekSample/WorkTime.cs
@@ -6,6 +6,11 @@ using System.Text.Json.Serialization;
 
 namespace CdekSample;
 
+/// <summary>
+/// Opening hours for every day
+/// </summary>
+/// <param name="Day">Ordinal number of a day (Monday = 1, Sunday = 7)</param>
+/// <param name="Time">Opening hours for these days. If the pickup point does not work on this day, no period is specified</param>
 public sealed record class WorkTime
 (
   [property: JsonPropertyName("day")]  int    Day,
