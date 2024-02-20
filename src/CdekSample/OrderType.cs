06b3f937ede63d90f9a23bc565df6a100a7ff525
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-20 12:03:27 -0800

    add order type

---
+++ src/CdekSample/OrderType.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace CdekSample;
+
+public readonly struct OrderType
+{
+  private OrderType(int code) => Code = code;
+
+  private int Code { get; }
+
+  private static OrderType From(int code)
+  {
+    if (code == OrderType.None       ) return OrderType.None;
+    if (code == OrderType.OnlineStore) return OrderType.OnlineStore;
+    if (code == OrderType.Delivery   ) return OrderType.Delivery;
+
+    throw new Exception("Invalid code to create OrderType");
+  }
+
+  public static implicit operator OrderType(int code)      => OrderType.From(code);
+  public static implicit operator int(OrderType orderType) => orderType.Code;
+
+  public static readonly OrderType None        = new(0);
+  public static readonly OrderType OnlineStore = new(1);
+  public static readonly OrderType Delivery    = new(2);
+}
