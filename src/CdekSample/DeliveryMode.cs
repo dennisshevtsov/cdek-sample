4a9dab714353490d276bb8d34014ccce244dd89b
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-02-23 08:17:52 -0800

    add delivery terminal

---
+++ src/CdekSample/DeliveryMode.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace CdekSample;
+
+/// <summary>
+/// Режим доставки
+/// </summary>
+public readonly record struct DeliveryMode
+{
+  public DeliveryMode(): this(code: 0, name: "None") { }
+ 
+  private DeliveryMode(int code, string name) => (Code, Name) = (code, name);
+
+  private int Code { get; }
+
+  /// <summary>
+  /// Для логгирования
+  /// </summary>
+  private string Name { get; }
+
+  public override string ToString() => Name;
+
+  public static DeliveryMode From(int code)
+  {
+    if (code == DeliveryMode.None)                         return DeliveryMode.None;
+    if (code == DeliveryMode.DoorDoor)                     return DeliveryMode.DoorDoor;
+    if (code == DeliveryMode.DoorWarehouse)                return DeliveryMode.DoorWarehouse;
+    if (code == DeliveryMode.WarehouseDoor)                return DeliveryMode.WarehouseDoor;
+    if (code == DeliveryMode.WarehouseWarehouse)           return DeliveryMode.WarehouseWarehouse;
+    if (code == DeliveryMode.DoorParcelTeminal)            return DeliveryMode.DoorParcelTeminal;
+    if (code == DeliveryMode.WarehouseParcelTerminal)      return DeliveryMode.WarehouseParcelTerminal;
+    if (code == DeliveryMode.ParcelTerminalDoor)           return DeliveryMode.ParcelTerminalDoor;
+    if (code == DeliveryMode.ParcelTerminalWarehouse)      return DeliveryMode.ParcelTerminalWarehouse;
+    if (code == DeliveryMode.ParcelTerminalParcelTerminal) return DeliveryMode.ParcelTerminalParcelTerminal;
+
+    throw new Exception($"Invalid code to create DeliveryMode: {code}");
+  }
+
+  public static implicit operator DeliveryMode(int code)  => DeliveryMode.From(code);
+  public static implicit operator int(DeliveryMode value) => value.Code;
+
+  /// <summary>
+  /// Значение по умолчанию
+  /// </summary>
+  public static readonly DeliveryMode None = new();
+
+  /// <summary>
+  /// дверь-дверь
+  /// </summary>
+  public static readonly DeliveryMode DoorDoor = new(code: 1, name: "DoorDoor");
+
+  /// <summary>
+  /// дверь-склад
+  /// </summary>
+  public static readonly DeliveryMode DoorWarehouse = new (code: 2, name: "DoorWarehouse");
+
+  /// <summary>
+  /// склад-дверь
+  /// </summary>
+  public static readonly DeliveryMode WarehouseDoor = new (code: 3, name: "WarehouseDoor");
+
+  /// <summary>
+  /// склад-склад
+  /// </summary>
+  public static readonly DeliveryMode WarehouseWarehouse = new (code: 4, name: "WarehouseWarehouse");
+
+  /// <summary>
+  /// дверь-постамат
+  /// </summary>
+  public static readonly DeliveryMode DoorParcelTeminal = new (code: 6, name: "DoorParcelTeminal");
+
+  /// <summary>
+  /// склад-постамат
+  /// </summary>
+  public static readonly DeliveryMode WarehouseParcelTerminal = new (code: 7, name: "WarehouseParcelTerminal");
+
+  /// <summary>
+  /// постамат-дверь
+  /// </summary>
+  public static readonly DeliveryMode ParcelTerminalDoor = new (code: 8, name: "ParcelTerminalDoor");
+
+  /// <summary>
+  /// постамат-склад
+  /// </summary>
+  public static readonly DeliveryMode ParcelTerminalWarehouse = new (code: 9, name: "ParcelTerminalWarehouse");
+
+  /// <summary>
+  /// постамат-постамат
+  /// </summary>
+  public static readonly DeliveryMode ParcelTerminalParcelTerminal = new (code: 10, name: "ParcelTerminalParcelTerminal");
+}
