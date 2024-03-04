75ecb6b17b378eccd8a5ac8be718894323a4d8c9
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-04 11:33:02 -0800

    add additional service response

---
+++ src/CdekSample/AdditionalServiceResponse.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class AdditionalServiceResponse
+(
+  [property: JsonPropertyName("code")] AdditionalServiceType Code,
+  [property: JsonPropertyName("sum")]                  float Sum
+);
