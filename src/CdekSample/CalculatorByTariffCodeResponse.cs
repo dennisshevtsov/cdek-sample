a0d5b89fb36d8c0179d29e8e8d4927b3759bbca0
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-04 11:33:35 -0800

    add calculator response

---
+++ src/CdekSample/CalculatorByTariffCodeResponse.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public sealed record class CalculatorByTariffCodeResponse
+(
+  [property: JsonPropertyName("delivery_sum")]                  float DeliverySum,
+  [property: JsonPropertyName("period_min")]                      int PeriodMin,
+  [property: JsonPropertyName("period_max")]                      int PeriodMax,
+  [property: JsonPropertyName("weight_calc")]                     int WeightCalc,
+  [property: JsonPropertyName("calendar_min")]                   int? CalendarMin,
+  [property: JsonPropertyName("calendar_max")]                   int? CalendarMax,
+  [property: JsonPropertyName("services")] AdditionalServiceResponse? Services,
+  [property: JsonPropertyName("total_sum")]                     float TotalSum,
+  [property: JsonPropertyName("currency")]                   Currency Currency
+);
