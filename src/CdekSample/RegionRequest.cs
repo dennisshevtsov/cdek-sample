78ff7acd610e3da25962c01f96b24b8700b2c145
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-10 00:48:29 -0800

    add region request

---
+++ src/CdekSample/RegionRequest.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace CdekSample;
+
+public sealed record class RegionRequest
+(
+  string[]? CountryCodes = null, // country_codes
+  int?      Size         = null, // size
+  int?      Page         = null, // page
+  Language? Language     = null  // lang
+);
