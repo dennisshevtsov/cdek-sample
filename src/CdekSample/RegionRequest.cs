2ea9e890e0810cc11b0b33af9cdc40abdb403d67
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-10 00:58:33 -0800

    implement touri

--- src/CdekSample/RegionRequest.cs
+++ src/CdekSample/RegionRequest.cs
@@ -6,12 +6,14 @@ namespace CdekSample;
 
 public sealed record class RegionRequest
 (
-  string[]? CountryCodes = null, // country_codes
-  int?      Size         = null, // size
-  int?      Page         = null, // page
-  Language? Language     = null  // lang
+  string[]? CountryCodes = null,
+  int?      Size         = null,
+  int?      Page         = null,
+  Language? Language     = null
 )
 {
+  public const string Route = "v2/location/regions";
+
   public string[]? CountryCodes { get; } = CountryCodes;
 
   public int? Size { get; } = Size is null && Page is not null ? throw new Exception("Size required if page not null") : Size;
@@ -19,4 +21,39 @@ public sealed record class RegionRequest
   public int? Page { get; } = Page;
 
   public Language? Language { get; } = Language;
+
+  public string ToUri()
+  {
+    List<string> segments = [Route, "?"];
+
+    if (CountryCodes is not null)
+    {
+      for (int i = 0; i < CountryCodes.Length; i++)
+      {
+        segments.Add("country_codes=");
+        segments.Add(CountryCodes[i]);
+      }
+    }
+
+    if (Size is not null)
+    {
+      segments.Add("size=");
+      segments.Add(Size.Value.ToString());
+    }
+
+    if (Page is not null)
+    {
+      segments.Add("page=");
+      segments.Add(Page.Value.ToString());
+    }
+
+    if (Language is not null)
+    {
+      segments.Add("lang=");
+      segments.Add(Language.Value.ToString());
+    }
+
+    segments.RemoveAt(segments.Count - 1);
+    return string.Concat(segments);
+  }
 }
