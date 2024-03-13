020fab5b60d8321f5ea9561d82452f32b27371c1
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-13 12:39:59 -0700

    add city

---
+++ src/CdekSample/City.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace CdekSample;
+
+public readonly struct City
+{
+  private City(int code) => Code = code;
+
+  private int Code { get; }
+
+  public override string ToString() => Code.ToString();
+
+  private static City From(int code)
+  {
+    if (code <= 0)
+    {
+      throw new Exception($"Invalid code {code} to create City");
+    }
+
+    return new(code);
+  }
+
+  public static implicit operator City(int code) => City.From(code);
+  public static implicit operator int(City city) => city.Code;
+}
