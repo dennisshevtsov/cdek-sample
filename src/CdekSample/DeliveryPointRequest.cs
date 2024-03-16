080f2e2b42074685657d5c79529c057720ffccf0
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-16 10:19:17 -0700

    add delivery point request

--- src/CdekSample/DeliveryPointRequest.cs
+++ src/CdekSample/DeliveryPointRequest.cs
@@ -24,7 +24,177 @@ public sealed record DeliveryPointRequest
   Guid?              FiasGuid,
   string?            Code,
   bool?              IsTtl,
-  bool?              IsFoolfillment,
+  bool?              Foolfillment,
   int?               Size,
   int?               Page
-);
+)
+{
+  public const string Route = "v2/deliverypoints";
+  public string ToUri()
+  {
+    // 22 params by 3 segments (param_name= param_value &)
+    // 1 route
+    // 1 ?
+    List<string> segments = new(capacity: 22 * 3 + 1 + 1)
+    {
+      DeliveryPointRequest.Route,
+      "?",
+    };
+
+    if (PostalCode is not null)
+    {
+      segments.Add("postal_code=");
+      segments.Add(PostalCode);
+      segments.Add("&");
+    }
+
+    if (City is not null)
+    {
+      segments.Add("city_code=");
+      segments.Add(City);
+      segments.Add("&");
+    }
+
+    if (Type is not null)
+    {
+      segments.Add("type=");
+      segments.Add(Type);
+      segments.Add("&");
+    }
+
+    if (Country is not null)
+    {
+      segments.Add("country_code=");
+      segments.Add(Country);
+      segments.Add("&");
+    }
+
+    if (RegionCode is not null)
+    {
+      segments.Add("region_code=");
+      segments.Add(RegionCode.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (HaveCashless is not null)
+    {
+      segments.Add("have_cashless=");
+      segments.Add(HaveCashless.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (PostalCode is not null)
+    {
+      segments.Add("postal_code=");
+      segments.Add(PostalCode);
+      segments.Add("&");
+    }
+
+    if (HaveCach is not null)
+    {
+      segments.Add("have_cash=");
+      segments.Add(HaveCach.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (AllowedCod is not null)
+    {
+      segments.Add("allowed_cod=");
+      segments.Add(AllowedCod.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (IsDressingRoom is not null)
+    {
+      segments.Add("is_dressing_room=");
+      segments.Add(IsDressingRoom.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (WeightMax is not null)
+    {
+      segments.Add("weight_max=");
+      segments.Add(WeightMax.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (WeightMin is not null)
+    {
+      segments.Add("weight_min=");
+      segments.Add(WeightMin.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (Language is not null)
+    {
+      segments.Add("lang=");
+      segments.Add(Language);
+      segments.Add("&");
+    }
+
+    if (TakeOnly is not null)
+    {
+      segments.Add("take_only=");
+      segments.Add(TakeOnly.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (IsHandout is not null)
+    {
+      segments.Add("is_handout=");
+      segments.Add(IsHandout.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (IsReception is not null)
+    {
+      segments.Add("is_reception=");
+      segments.Add(IsReception.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (FiasGuid is not null)
+    {
+      segments.Add("fias_guid=");
+      segments.Add(FiasGuid.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (Code is not null)
+    {
+      segments.Add("code=");
+      segments.Add(Code);
+      segments.Add("&");
+    }
+
+    if (IsTtl is not null)
+    {
+      segments.Add("is_ltl=");
+      segments.Add(IsTtl.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (Foolfillment is not null)
+    {
+      segments.Add("fulfillment=");
+      segments.Add(Foolfillment.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (Size is not null)
+    {
+      segments.Add("size=");
+      segments.Add(Size.Value.ToString());
+      segments.Add("&");
+    }
+
+    if (Page is not null)
+    {
+      segments.Add("page=");
+      segments.Add(Page.Value.ToString());
+      segments.Add("&");
+    }
+
+    return string.Concat(segments);
+  }
+}
