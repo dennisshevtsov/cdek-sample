beb8194b9dc88ab22fa557929344eb3b08548d6d
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-18 14:11:34 -0700

    add json converter

--- src/CdekSample/RegionCode.cs
+++ src/CdekSample/RegionCode.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace CdekSample;
 
+[JsonConverter(typeof(RegionCodeJsonConverter))]
 public readonly struct RegionCode
 {
   private RegionCode(string code) => Code = code;
@@ -12,6 +15,16 @@ public readonly struct RegionCode
 
   public override string ToString() => Code;
 
-  public static implicit operator RegionCode(string code)       => new(code);
+  private static RegionCode From(string? code)
+  {
+    if (string.IsNullOrEmpty(code))
+    {
+      throw new Exception("Unempty code required to create RegionCode");
+    }
+
+    return new(code);
+  }
+
+  public static implicit operator RegionCode(string? code)      => RegionCode.From(code);
   public static implicit operator string(RegionCode regionCode) => regionCode.Code;
 }
