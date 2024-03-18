bdc5836d607a124285fb3904e97d7c6bde5f67bc
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-18 14:07:07 -0700

    add region code

---
+++ src/CdekSample/RegionCode.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace CdekSample;
+
+public readonly struct RegionCode
+{
+  private RegionCode(string code) => Code = code;
+
+  private string Code { get; }
+
+  public override string ToString() => Code;
+
+  public static implicit operator RegionCode(string code)       => new(code);
+  public static implicit operator string(RegionCode regionCode) => regionCode.Code;
+}
