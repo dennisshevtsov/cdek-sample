474cf939a5323dc84601b4eaea1a18d82be7e2eb
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-22 10:05:29 -0700

    add money json converter

--- src/CdekSample/Money.cs
+++ src/CdekSample/Money.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.Text.Json.Serialization;
+
 namespace CdekSample;
 
+[JsonConverter(typeof(MoneyJsonConverter))]
 public readonly record struct Money(decimal Value, decimal? VatSum, VatRate? VatRate);
---
+++ src/CdekSample/MoneyJsonConverter.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace CdekSample;
+
+public sealed class MoneyJsonConverter : JsonConverter<Money>
+{
+  public override Money Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+    throw new NotImplementedException();
+
+  public override void Write(Utf8JsonWriter writer, Money value, JsonSerializerOptions options)
+  {
+    writer.WriteStartObject();
+    writer.WriteNumber("value", value.Value);
+   
+    if (value.VatSum is not null)
+    {
+      writer.WriteNumber("vat_sum", value.VatSum.Value);
+    }
+    else
+    {
+      writer.WriteNull("vat_sum");
+    }
+
+    if (value.VatRate is not null)
+    {
+      writer.WriteNumber("vat_rate", value.VatRate.Value);
+    }
+    else
+    {
+      writer.WriteNull("vat_rate");
+    }
+
+    writer.WriteEndObject();
+  }
+}
