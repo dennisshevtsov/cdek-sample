f39397c94db1d77701544f4167e7c0100b5b264d
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-03-23 07:55:09 -0700

    add threshold

---
+++ src/CdekSample/Threshold.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using System.Text.Json.Serialization;
+
+namespace CdekSample;
+
+public readonly record struct Threshold
+(
+  [property: JsonPropertyName("threshold")] int Value,
+  [property: JsonPropertyName("sum")] decimal Sum,
+
+  [property: JsonPropertyName("vat_sum"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+  decimal? VatSum = null,
+
+  [property: JsonPropertyName("vat_rate"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+  VatRate? VatRate = null
+);
